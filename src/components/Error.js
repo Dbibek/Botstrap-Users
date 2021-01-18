ee50739206f9bc977293e850300b07be58c2f9ee
Bibek Dhakal <47821826+Dbibek@users.noreply.github.com>
2021-01-18 23:53:50 +0200

    error handeling refactored

--- src/components/Error.js
+++ src/components/Error.js
@@ -2,8 +2,9 @@ import React from "react";
 
 const ErrorCard = (error) => {
   return (
-    <div class="alert alert-danger" role="alert">
-      {error}
+    <div className="alert alert-danger" role="alert">
+      <h3 className="alert-heading">{error}</h3>
+      <h1>there could be problem in fetching data</h1>
     </div>
   );
 };
